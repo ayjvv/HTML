e00c1712df2442f03c69783db98f16da1775fc36
ajyvv <ayjvv@naver.com>
2024-04-29 17:49:10 +0900

    수정

---
+++ 수업기록/240429_js/함수Ex7.js
@@ -0,0 +1,6 @@
+function tomato() {
+  document.querySelector('body').style.background = 'tomato';
+}
+const orange = () => document.body.style.backgroundColor = 'orange';
+
+//css 보다 더 높은 우선순위를 가진다
